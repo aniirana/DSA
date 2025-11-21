78249c98c1edfbe36ef9a977a5075953ad36401b
aniirana <anirudhrana151@gmail.com>
2025-11-21 19:54:03 +0530

    Time: 9 ms (37.46%) | Memory: 55.6 MB (16.22%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+
+        int count=1;
+        
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                count++;
+            }
+
+            else{
+                count=1;
+            }
+
+            if(count>(nums.length/2)){
+                return nums[i];
+            }
+
+        }
+        return nums[0];
+    }
+}
