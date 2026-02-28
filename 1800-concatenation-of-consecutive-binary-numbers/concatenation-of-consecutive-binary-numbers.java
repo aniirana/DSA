7b5820f40d115ce6300b2d16c319a9806bf27b5a
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-02-28 13:37:01 +0530

    Time: 25 ms (65.12%) | Memory: 42.4 MB (73.26%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        long ans = 0;
+        int mod = 1000000007;
+        int bits = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Check power of 2
+            if ((i & (i - 1)) == 0) {
+                bits++;
+            }
+
+            ans = ((ans << bits) | i) % mod;
+        }
+
+        return (int) ans;
+    }
+}
