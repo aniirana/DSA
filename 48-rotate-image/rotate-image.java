5af0e25759663a0c5a42a3dce538f7313b1b273d
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-05-04 14:29:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (84.65%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        //transpose of matrix 
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix.length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+
+        }
+
+
+        int j=matrix.length;
+        //reversing a matrix
+        for (int i=0;i<matrix.length;i++){
+            int left=0;
+            int right=j-1;
+            
+            while(left<right){
+                int temp=matrix[i][left];
+            matrix[i][left]=matrix[i][right];
+            matrix[i][right]=temp;
+
+            left++;
+            right--;
+            }
+            
+            
+        }
+
+      
+    }
+}
