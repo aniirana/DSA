4cea937b3f8b71573763f97f51c0cb981f5b19b7
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-06-06 19:29:09 +0530

    Time: 1 ms (99.68%) | Memory: 47.2 MB (55.08%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps=0, left=0,right=0;
+        
+        while(right<nums.length-1){
+            int farthest=0;
+
+            for(int i=left;i<=right;i++){
+                farthest=Math.max(i+nums[i],farthest);
+            }
+            left=right+1;
+            right=farthest;
+            jumps+=1;
+        }
+        return jumps;
+        
+    }
+}
