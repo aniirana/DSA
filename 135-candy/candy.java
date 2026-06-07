a6f70de5613d7194443f3261d00c38530fce2f14
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-06-07 18:08:08 +0530

    Time: 2 ms (99.65%) | Memory: 47.8 MB (15.63%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int sum=1,i=1;
+        while(i<ratings.length){
+            if(ratings[i]==ratings[i-1]){
+                sum+=1;
+                i++;
+                continue;
+            }
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak+=1;
+                sum+=peak;
+                i++;
+            }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                down+=1;
+                i++;
+            }
+
+            if(down>peak){
+                sum+=down-peak;
+            }
+        }
+        return sum;
+    }
+}
