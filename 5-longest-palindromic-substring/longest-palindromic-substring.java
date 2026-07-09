9e1f3bd4157198adeceb67a5df11ffef076dbde4
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-07-09 10:40:02 +0530

    Time: 166 ms (22.25%) | Memory: 48.8 MB (5.49%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,42 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
+        int n=s.length();
+        boolean[][] dp=new boolean[n+1][n+1];
+        int start=0;
+        int maxlen=1;
 
-        int m = s.length();
+        for(int gap=0;gap<n;gap++){
+            for(int i=0,j=gap;j<n;i++,j++){
+                if(gap==0){
+                    dp[i][j]=true;
+                }
+                else if(gap==1){
+                    dp[i][j]=(s.charAt(i)==s.charAt(j));
+                        
+                    
+                }else{
+                    dp[i][j]=(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]);
+                }
 
-        String t = new StringBuilder(s).reverse().toString();
-        int n = t.length();
-
-        int[][] dp = new int[m + 1][n + 1];
-
-        int maxLen = 0;
-        int endIndex = 0;
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-
-                if (s.charAt(i - 1) == t.charAt(j - 1)) {
-
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-
-                    int len = dp[i][j];
-
-                    // Starting index in original string
-                    int originalStart = i - len;
-
-                    // Corresponding starting index in original string
-                    int reversedStart = m - j;
-
-                    if (originalStart == reversedStart && len > maxLen) {
-                        maxLen = len;
-                        endIndex = i;
-                    }
-
-                } else {
-                    dp[i][j] = 0;
+                if(dp[i][j] && gap+1>maxlen){
+                    start=i;
+                    maxlen=gap+1;
                 }
             }
         }
 
-        return s.substring(endIndex - maxLen, endIndex);
+        return s.substring(start,start+maxlen);
+        
     }
 }
