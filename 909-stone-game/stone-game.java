f7cd34c9c4b262c69f09dc568a1580d4204ce775
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-07-14 12:38:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (95.11%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,15 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int n=piles.length;
-        int[][] dp=new int[n][n];
-         for (int i = 0; i < n; i++)
-            dp[i][i] = piles[i];
-
-        for(int gap=1;gap<n;gap++){
-            for(int i=0,j=gap;j<n;i++,j++){
-                dp[i][j]=Math.max(piles[i]-dp[i+1][j],piles[j]-dp[i][j-1]);
-            }
-        }
-        return dp[0][n-1]>0;
+        return true;
     }
 }
