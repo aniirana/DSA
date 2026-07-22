afc75041b579663c572dfdeef90d7dc79b470cb1
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-07-22 15:00:49 +0530

    Time: 5 ms (92.89%) | Memory: 49.5 MB (25.32%) - LeetSync

--- 401-binary-watch/binary-watch.java
+++ 401-binary-watch/binary-watch.java
@@ -1,36 +1,18 @@
 class Solution {
-    List<String> ans=new ArrayList<>();
-    int[] min={1,2,4,8,16,32};
-    int[] hr={1,2,4,8};
     public List<String> readBinaryWatch(int turnedOn) {
-        
-        solve(0,turnedOn,0,0);
-        return ans;
-    }
+        List<String> ans = new ArrayList<>();
 
-    public void solve(int idx,int k,int hrs,int mins){
-        if(k==0){
-            if(mins<=59 && hrs<=11){
-                ans.add(hrs+":"+(mins<10?"0"+mins:mins));
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+
+                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+
+                    String time = h + ":" + (m < 10 ? "0" + m : m);
+                    ans.add(time);
+                }
             }
-            return;
-            
-        }
-        if(idx==10){
-            return;
         }
 
-       
-        //not take
-        solve(idx+1,k,hrs,mins);
-
-        //take
-        if(idx<4){
-            solve(idx+1,k-1,hrs+hr[idx],mins);
-        }else{
-            solve(idx+1,k-1,hrs,mins+min[idx-4]);
-        }
-      
-       
+        return ans;
     }
 }
