41cdf31493048d1fd02244d4b4a4880618b19529
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-07-22 11:19:46 +0530

    Time: 38 ms (66.81%) | Memory: 99 MB (11.00%) - LeetSync

--- 456-132-pattern/132-pattern.java
+++ 456-132-pattern/132-pattern.java
@@ -6,18 +6,16 @@ class Solution {
         int third=Integer.MIN_VALUE;
         int prevlarg=Integer.MIN_VALUE;
         for(int i=n-1;i>=0;i--){
-            prevlarg=Math.max(prevlarg,nums[i]);
-            if(st.isEmpty()){
-                st.push(nums[i]);
+            if(nums[i]<third){
+                return true;
             }
+            
 
             while(!st.isEmpty() && st.peek()<nums[i]){
                 third=Math.max(third,st.pop());
             }
 
-            if(nums[i]<third && third<prevlarg){
-                return true;
-            }
+            
 
             st.push(nums[i]);    
         }
