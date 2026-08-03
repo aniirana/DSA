3982dec972c0e05305e6d870eb78a879d40126fa
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-08-03 14:32:38 +0530

    Time: 3 ms (70.67%) | Memory: 45.6 MB (13.33%) - LeetSync

--- 2754-maximum-strength-of-a-group/maximum-strength-of-a-group.java
+++ 2754-maximum-strength-of-a-group/maximum-strength-of-a-group.java
@@ -1,46 +1,38 @@
 class Solution {
     public long maxStrength(int[] nums) {
-        int n=nums.length;
-         if (n == 1) return nums[0];
         Arrays.sort(nums);
-        long product = 1;
-        int negCount = 0;
-        int zeroCount = 0;
+        int n=nums.length;
+        int zerocount=0;
+        int negcount=0;
 
-        for (int num : nums) {
-            if (num < 0) negCount++;
-            else if (num == 0) zeroCount++;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0) zerocount++;
+
+            if(nums[i]<0) negcount++;
         }
 
-
-        int skip = -1;
-        if (negCount % 2 == 1) {
-            skip = negCount - 1;   // index of largest negative
+        int skip=-1;
+        if(negcount%2==1){
+            skip=negcount-1;
         }
 
-        boolean taken = false;
-
-        for (int i = 0; i < n; i++) {
-            if (i == skip) continue;
-            if (nums[i] == 0) continue;
-
-            product *= nums[i];
-            taken = true;
+        if(n == 1){
+           return nums[0];
         }
 
-        // If nothing was taken
-        if (!taken) {
-            // If zero exists, answer is 0
-            if (zeroCount > 0) return 0;
+        long prod=1;
+        boolean taken=false;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0 || i==skip) continue;
 
-            // Otherwise all numbers were negative and only one exists
-            return nums[n - 1];
+            
+
+            prod=prod*nums[i];
+            taken=true;
         }
-
-        return product;
-    
-        
-
-        
+        if(!taken){
+            return 0;
+        }
+        return prod;
     }
 }
