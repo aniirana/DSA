47e2cc0a8eba1ca793e453b2cefd8e2ab713ab5c
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-08-04 12:22:53 +0530

    Time: 2 ms (66.63%) | Memory: 46.7 MB (99.63%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -2,47 +2,43 @@ class Solution {
     public List<String> letterCasePermutation(String s) {
         List<String> ans=new ArrayList<>();
 
-        solve(0,ans,s,new StringBuilder());
-
+        solve(0,s,ans,new StringBuilder());
         return ans;
     }
 
-    public void solve(int idx,List<String> ans,String s,StringBuilder str){
-        if(idx==s.length()){
-            ans.add(str.toString());
+    public void solve(int i,String s,List<String> ans,StringBuilder sb){
+        int n=s.length();
+        if(i==n){
+            ans.add(sb.toString());
             return;
         }
 
         char[] arr=s.toCharArray();
 
-        if(isDigit(arr[idx])){
-            str.append(arr[idx]);
-            
-            solve(idx+1,ans,s,str);
-            str.deleteCharAt(str.length() - 1);
+        if(isnum(arr[i])){
+            sb.append(arr[i]);
+            solve(i+1,s,ans,sb);
+            sb.deleteCharAt(sb.length()-1);
             return;
-        } 
+        }
+
+        sb.append(Character.toLowerCase(arr[i]));
+        solve(i+1,s,ans,sb);
+        sb.deleteCharAt(sb.length()-1);
+
+        sb.append(Character.toUpperCase(arr[i]));
+        solve(i+1,s,ans,sb);
+        sb.deleteCharAt(sb.length()-1);
+
 
         
-        str.append(Character.toLowerCase(arr[idx]));
-        solve(idx+1,ans,s,str);
-        str.deleteCharAt(str.length() - 1);
-
-        
-        str.append(Character.toUpperCase(arr[idx]));
-        solve(idx+1,ans,s,str);
-        str.deleteCharAt(str.length() - 1);
-
-
 
     }
 
-
-    public boolean isDigit(char ch){
-        if(ch=='0' || ch=='1' || ch=='2' || ch=='3' || ch=='4' || ch=='5' ||
-            ch=='6' || ch== '7' || ch=='8' || ch=='9'){
-                return true;
-            }
+    public boolean isnum(char ch){
+        if(ch=='0' || ch=='1' ||ch=='2' ||ch=='3' ||ch=='4' ||ch=='5' ||ch=='6' ||ch=='7' ||ch=='8' ||ch=='9'){
+            return true;
+        }
 
         return false;
     }
