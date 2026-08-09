66b49db57595c8d5d0f8e601b4871f33d024b60d
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-08-09 12:01:23 +0530

    Time: 12 ms (36.12%) | Memory: 45.1 MB (44.49%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.java
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -1,41 +1,45 @@
 class Solution {
     public int stoneGameII(int[] piles) {
+        int n=piles.length;
+        int[][][] dp=new int[2][n+1][n+1];
 
-        int n = piles.length;
-
-        int[][] dp = new int[n + 1][n + 1];
-
-        for(int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], Integer.MIN_VALUE);
-        }
-
-        for(int M = 1; M <= n; M++) {
-            dp[n][M] = 0;
-        }
-
-        int[] suffix = new int[n + 1];
-
-        for(int i = n - 1; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] + piles[i];
-        }
-
-        for(int i = n - 1; i >= 0; i--) {
-
-            for(int M = 1; M <= n; M++) {
-
-                for(int x = 1; x <= 2 * M && i + x <= n; x++) {
-
-                    int newM = Math.max(M, x);
-
-                    int taken = suffix[i] - suffix[i + x];
-
-                    int current = taken - dp[i + x][newM];
-
-                    dp[i][M] = Math.max(dp[i][M], current);
+        for(int i=0;i<2;i++){
+            for(int j=0;j<n+1;j++){
+                for(int k=0;k<n+1;k++){
+                    dp[i][j][k]=-1;
                 }
             }
         }
+        
+       return solve(1,0,1,piles,dp);
+    }
 
-        return (suffix[0] + dp[0][1]) / 2;
+    public int solve(int person,int i,int m,int[] piles,int[][][] dp){
+        int n=piles.length;
+
+        if(i>=n){
+            return 0;
+        }
+
+        if(dp[person][i][m]!= -1){
+            return dp[person][i][m];
+        }
+        int sum=0;
+        int res=(person==1)?Integer.MIN_VALUE:Integer.MAX_VALUE;
+
+        
+
+        for(int x=1;x<=Math.min(2*m,n-i);x++){
+            sum+=piles[i+x-1];
+
+            if(person==1){
+                res=Math.max(res,sum+solve(0,i+x,Math.max(m,x),piles,dp));
+            }else{
+                res=Math.min(res,solve(1,i+x,Math.max(m,x),piles,dp));
+            }
+        }
+
+        return dp[person][i][m]=res;
+
     }
 }
