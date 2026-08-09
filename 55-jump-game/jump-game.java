a48dc5d256e3423deddc5b33fbd5e332abc211fc
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-08-09 12:19:19 +0530

    Time: 2 ms (88.76%) | Memory: 47.8 MB (81.87%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxidx=0;
-        for(int i=0;i<nums.length;i++){
-            if(i>maxidx) return false;
-            else{
-                maxidx=Math.max(maxidx,i+nums[i]);
+        int max=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(i>max) return false;
+            else {
+                max=Math.max(max,i+nums[i]);
             }
         }
         return true;
-        
     }
 }
