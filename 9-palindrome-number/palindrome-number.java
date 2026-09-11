cc6382424ff1683feffe9a053e406cccdf1a6228
Anirudh Rana <Anirudh.119643@stu.upes.ac.in>
2026-09-11 23:08:06 +0530

    Time: 5 ms (82.56%) | Memory: 45.5 MB (96.47%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rem;
+        int temp=x;
+        int rev=0;
+        if(x<0){
+            return false;
+        }
+        else{
+            while(x>0){
+                rem=x%10;
+                rev=(rev*10)+rem;
+                x=x/10;
+            }
+        if(temp==rev){
+            return true;
+        }   
+        else
+            return false;
+        }
+    }
+}
